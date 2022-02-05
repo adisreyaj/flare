4737126ad64893e9844a9ced7a9c091fda335f2e
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-02-05 16:05:20 +0530

    feat(ui): fix errors`

--- apps/flare/src/app/app.component.ts
+++ apps/flare/src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Message } from '@flare/api-interfaces';
 
 @Component({
   selector: 'flare-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
-}
+export class AppComponent {}
