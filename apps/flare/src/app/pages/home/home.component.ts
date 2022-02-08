c2ca0ba149ec273320f1982d413b83916c134644
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-02-08 23:27:48 +0530

    feat: update config and styles

--- apps/flare/src/app/pages/home/home.component.ts
+++ apps/flare/src/app/pages/home/home.component.ts
@@ -1,20 +1,41 @@
 import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { SidebarComponentModule } from '@flare/ui/components';
+import {
+  ComposerModule,
+  FlareCardModule,
+  SidebarComponentModule,
+} from '@flare/ui/components';
 
 @Component({
   selector: 'flare-home',
   template: `<aside>
       <flare-sidebar></flare-sidebar>
     </aside>
-    <main></main>
-    <aside></aside>`,
+    <main class="border-r border-slate-100 overflow-y-auto">
+      <flare-composer></flare-composer>
+      <ul>
+        <li>
+          <flare-card></flare-card>
+        </li>
+        <li>
+          <flare-card></flare-card>
+        </li>
+        <li>
+          <flare-card></flare-card>
+        </li>
+        <li>
+          <flare-card></flare-card>
+        </li>
+      </ul>
+    </main>
+    <aside hidden></aside>`,
   styles: [
+    //language=SCSS
     `
       :host {
         display: grid;
-        grid-template-columns: 280px 1fr 200px;
+        grid-template-columns: 280px 1fr;
         height: 100vh;
         grid-template-rows: 1fr;
       }
@@ -29,6 +50,9 @@ export class HomeComponent {}
     CommonModule,
     RouterModule.forChild([{ path: '', component: HomeComponent }]),
     SidebarComponentModule,
+    ComposerModule,
+    FlareCardModule,
+    FlareCardModule,
   ],
   exports: [HomeComponent],
 })
