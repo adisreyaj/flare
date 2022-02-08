c31c5b56b10fc932a114a0d1b337c2e9b942c4f4
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-02-08 23:27:48 +0530

    feat: update config and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-  content: ['libs/ui/**/*.{ts,html}', 'zigzag/**/*.{ts,html}'],
+  content: [
+    'apps/flare/src/**/*.{ts,html}',
+    'libs/ui/**/*.{ts,html}',
+    'zigzag/**/*.{ts,html}',
+  ],
   theme: {
     extend: {
       colors: {
         primary: 'var(--primary)',
+        'primary-transparent-10': 'var(--primary-transparent-10)',
       },
     },
   },
