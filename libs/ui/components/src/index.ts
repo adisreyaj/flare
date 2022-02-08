3877a4c3787d34ad6ff8b07856ead30db85a9027
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-02-08 23:27:25 +0530

    feat(components): update to sidebar

--- libs/ui/components/src/index.ts
+++ libs/ui/components/src/index.ts
@@ -1,2 +1,4 @@
 export * from './lib/sidebar/sidebar.component';
 export * from './lib/icon/icon.module';
+export * from './lib/composer/composer.component';
+export * from './lib/flare-card/flare-card.component';
