951451577e3523735694b0226928d0f4bd43f995
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-02-09 20:40:53 +0530

    feat(components): add text input component

---
+++ libs/ui/components/src/lib/flare-block-inputs/config.ts
@@ -0,0 +1,18 @@
+import { pellConfig } from 'pell';
+
+export const getPellEditorConfig = (
+  element: HTMLElement,
+  onChange: (html: string) => void
+): pellConfig<HTMLElement> => ({
+  defaultParagraphSeparator: 'p',
+  element,
+  onChange,
+  styleWithCSS: true,
+  actions: ['code'],
+  classes: {
+    actionbar: 'flare-composer-actionbar',
+    button: 'flare-composer-button',
+    content: 'flare-composer-content',
+    selected: 'flare-composer-selected',
+  },
+});
