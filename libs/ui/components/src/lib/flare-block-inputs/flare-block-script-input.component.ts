f2e51df10d526e9a820b30f15bf28c1cdee42f51
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-02-09 20:29:51 +0530

    feat(components): add script input component

---
+++ libs/ui/components/src/lib/flare-block-inputs/flare-block-script-input.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { EditorConfiguration } from 'codemirror';
+import {
+  ControlValueAccessor,
+  FormControl,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+import { BlockData, BlockType } from '@flare/api-interfaces';
+
+@Component({
+  selector: 'flare-block-script-input',
+  template: `<div
+    style="background-color: #292d3e;"
+    class="relative rounded-md p-2"
+  >
+    <div
+      class="absolute top-0 left-4 z-10 grid h-full place-items-center text-white"
+    >
+      <p>$</p>
+    </div>
+    <div class="pl-6">
+      <flare-code-input
+        mode="shell"
+        [value]="control.value"
+        [config]="codeInputConfig"
+        (valueChange)="handleValueChange($event)"
+      ></flare-code-input>
+    </div>
+  </div>`,
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `,
+  ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: FlareBlockScriptInputComponent,
+      multi: true,
+    },
+  ],
+})
+export class FlareBlockScriptInputComponent implements ControlValueAccessor {
+  codeInputConfig: EditorConfiguration = {
+    lineNumbers: false,
+  };
+
+  control = new FormControl(['']);
+  onChanged!: (value: BlockData) => void;
+  onTouched!: () => void;
+
+  handleValueChange(value: string) {
+    this.onChanged({ type: BlockType.script, content: value });
+  }
+
+  writeValue(value: BlockData): void {
+    this.control.setValue(value.content);
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChanged = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+}
