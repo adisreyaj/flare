ad5582605c34a951a9c9e64123a0bbd49eb3ab58
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-02-10 22:58:24 +0530

    feat(blocks-ui): add block renderer components

---
+++ libs/ui/components/src/lib/flare-block-renderers/flare-block-script-renderer.component.ts
@@ -0,0 +1,60 @@
+import { Component, Input, SecurityContext } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
+import { isNil } from 'lodash-es';
+import { EditorConfiguration } from 'codemirror';
+
+@Component({
+  selector: 'flare-block-script-renderer',
+  template: `<div
+    *ngIf="content$ | async as content"
+    style="background-color: #292d3e;"
+    class="relative rounded-md p-1"
+  >
+    <div
+      class="absolute top-0 left-4 z-10 grid h-full place-items-center text-sm text-white"
+    >
+      <p>$</p>
+    </div>
+    <div
+      class="absolute top-0 right-2 z-10 grid h-full place-items-center text-white"
+    >
+      <button
+        zzTooltip="Copy command"
+        [cdkCopyToClipboard]="content"
+        class="grid place-items-center rounded-full text-sm hover:text-primary"
+      >
+        <rmx-icon class="icon-xs" name="clipboard-line"></rmx-icon>
+      </button>
+    </div>
+    <div class="px-6">
+      <flare-code-input
+        style="font-size: 14px"
+        mode="shell"
+        [value]="content"
+        [config]="codeInputConfig"
+      ></flare-code-input>
+    </div>
+  </div>`,
+})
+export class FlareBlockScriptRendererComponent {
+  codeInputConfig: EditorConfiguration = {
+    lineNumbers: false,
+    readOnly: true,
+    inputStyle: 'contenteditable',
+  };
+
+  private readonly contentSubject = new BehaviorSubject<string>('');
+  public readonly content$ = this.contentSubject.asObservable();
+
+  constructor(private readonly sanitizer: DomSanitizer) {}
+  @Input()
+  set content(value: string | undefined | null) {
+    if (!isNil(value)) {
+      const sanitized = this.sanitizer.sanitize(SecurityContext.HTML, value);
+      if (sanitized) {
+        this.contentSubject.next(sanitized);
+      }
+    }
+  }
+}
