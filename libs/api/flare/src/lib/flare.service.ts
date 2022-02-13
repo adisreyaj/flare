45ed514abd0e251f4ec46afaa9e91a3fab61dae7
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-02-14 01:06:14 +0530

    fix(flare): flare without image fails

--- libs/api/flare/src/lib/flare.service.ts
+++ libs/api/flare/src/lib/flare.service.ts
@@ -15,10 +15,19 @@ import {
   InternalServerErrorException,
   Logger,
 } from '@nestjs/common';
-import { catchError, from, of, switchMap, tap, throwError } from 'rxjs';
+import {
+  catchError,
+  from,
+  of,
+  OperatorFunction,
+  switchMap,
+  tap,
+  throwError,
+} from 'rxjs';
 import { ApiMediaService } from '@flare/api/media';
 import { isNil } from 'lodash';
 import { getFlareFieldsToInclude } from './flare.helper';
+import { FileWithMeta } from '../../../media/src/lib/api-media.interface';
 
 @Injectable()
 export class FlareService {
@@ -49,58 +58,30 @@ export class FlareService {
   }
 
   create(flare: CreateFlareInput, user: CurrentUser) {
-    return from(this.mediaService.getJobData(flare.jobId)).pipe(
-      switchMap((data) => {
-        if (isNil(data)) {
-          this.logger.error('Job Data not found');
-          return throwError(
-            () => new InternalServerErrorException('Media upload error')
-          );
-        } else {
-          return from(this.mediaService.uploadToCloud(data.files));
-        }
-      }),
-      switchMap((data) => {
-        if (isNil(data)) {
-          return throwError(
-            () => new InternalServerErrorException('Media upload error')
-          );
-        } else {
-          return of(data);
-        }
-      }),
-      switchMap((data) => {
-        return from(
-          this.prisma.flare.create({
-            data: {
-              blocks: {
-                createMany: {
-                  data: flare.blocks.map((block) => ({
-                    ...block,
-                    ...(block.type === BlockType.images
-                      ? {
-                          content: (data ?? []).map((file) => ({
-                            name: file.filename,
-                            type: file.mimetype,
-                            size: file.size,
-                          })),
-                        }
-                      : {}),
-                  })),
-                },
-              },
-              deleted: false,
-              authorId: user.id,
-            },
-            include: getFlareFieldsToInclude(user.id),
-          })
+    const createFlare$ = (data: FileWithMeta[] = []) =>
+      from(
+        this.prisma.flare.create({
+          data: this.getCreateFlareData(flare, data, user),
+          include: getFlareFieldsToInclude(user.id),
+        })
+      ).pipe(
+        tap(() => {
+          this.mediaService.runJobImmediately(flare.jobId).then(() => {
+            this.logger.log(`Job ${flare.jobId} promoted to start immediately`);
+          });
+        })
+      );
+    /**
+     * If there are images in the flare, we need to upload them to the media service
+     * and then create the flare.
+     */
+    const query$ = isNil(flare.jobId)
+      ? createFlare$()
+      : from(this.mediaService.getJobData(flare.jobId)).pipe(
+          handleFileUploadsIfJobExists(),
+          switchMap((files) => createFlare$(files))
         );
-      }),
-      tap(() => {
-        this.mediaService.runJobImmediately(flare.jobId).then(() => {
-          this.logger.log(`Job ${flare.jobId} promoted to start immediately`);
-        });
-      }),
+    return query$.pipe(
       catchError((err) => {
         this.logger.error(err);
         if (err instanceof HttpException) {
@@ -214,4 +195,60 @@ export class FlareService {
       })
     );
   }
+
+  private getCreateFlareData(
+    flare: CreateFlareInput,
+    data: FileWithMeta[],
+    user: CurrentUser
+  ) {
+    return {
+      blocks: {
+        createMany: {
+          data: flare.blocks.map((block) => ({
+            ...block,
+            ...(block.type === BlockType.images
+              ? {
+                  content: (data ?? []).map((file) => ({
+                    name: file.filename,
+                    type: file.mimetype,
+                    size: file.size,
+                  })),
+                }
+              : {}),
+          })),
+        },
+      },
+      deleted: false,
+      authorId: user.id,
+    };
+  }
+}
+
+function handleFileUploadsIfJobExists(): OperatorFunction<
+  { files: FileWithMeta[] },
+  FileWithMeta[]
+> {
+  return function (source) {
+    return source.pipe(
+      switchMap((data) => {
+        if (isNil(data)) {
+          this.logger.error('Job Data not found');
+          return throwError(
+            () => new InternalServerErrorException('Media upload error')
+          );
+        } else {
+          return from(this.mediaService.uploadToCloud(data.files));
+        }
+      }),
+      switchMap((data: FileWithMeta[]) => {
+        if (isNil(data)) {
+          return throwError(
+            () => new InternalServerErrorException('Media upload error')
+          );
+        } else {
+          return of(data);
+        }
+      })
+    );
+  };
 }
