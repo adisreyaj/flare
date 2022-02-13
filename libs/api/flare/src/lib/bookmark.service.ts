e4bdc9fc1f9037b2fc436f768eebecd9df4cfe63
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-02-13 18:08:10 +0530

    feat(bookmark): add bookmark resolvers

--- libs/api/flare/src/lib/bookmark.service.ts
+++ libs/api/flare/src/lib/bookmark.service.ts
@@ -2,10 +2,30 @@ import { PrismaService } from '@flare/api/prisma';
 import { CurrentUser } from '@flare/api/shared';
 import { Injectable } from '@nestjs/common';
 import { from, map } from 'rxjs';
+import { getFlareFieldsToInclude } from './flare.helper';
 
 @Injectable()
 export class BookmarkService {
   constructor(private prisma: PrismaService) {}
+
+  findAllBookmarked(user: CurrentUser) {
+    return from(
+      this.prisma.bookmark.findMany({
+        where: {
+          authorId: user.id,
+        },
+        select: {
+          flare: {
+            include: getFlareFieldsToInclude(user.id),
+          },
+          createdAt: true,
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+      })
+    ).pipe(map((result) => result.map((bookmark) => bookmark.flare)));
+  }
 
   bookmark(flareId: string, user: CurrentUser) {
     return this.prisma.bookmark.create({
