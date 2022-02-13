81faed1f35fbcdc8d6c8d2d48f59bc7269eda20a
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-02-14 00:14:22 +0530

    feat(sidebar): make sidebar resposive

--- apps/flare/src/app/shell.component.ts
+++ apps/flare/src/app/shell.component.ts
@@ -23,8 +23,12 @@ import { User } from '@flare/api-interfaces';
       }
       .content {
         @apply mx-auto grid h-screen max-w-screen-xl;
-        grid-template-columns: 250px 1fr 250px;
+        grid-template-columns: 80px 1fr 250px;
         grid-template-rows: 1fr;
+
+        @media screen and (min-width: 768px) {
+          grid-template-columns: 250px 1fr 250px;
+        }
       }
     `,
   ],
