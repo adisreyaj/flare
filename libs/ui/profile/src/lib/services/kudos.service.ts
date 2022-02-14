bfc495aebcfb6b28cd067fbc89f8d526df6a1b3a
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-02-14 19:47:04 +0530

    feat(profile): give kudos modal added

--- libs/ui/profile/src/lib/services/kudos.service.ts
+++ libs/ui/profile/src/lib/services/kudos.service.ts
@@ -1,24 +1,49 @@
 import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
-import { map, Observable } from 'rxjs';
-import { Kudos } from '@flare/api-interfaces';
+import { map, mapTo, startWith, Subject, switchMap, tap } from 'rxjs';
+import { GiveKudosInput, Kudos } from '@flare/api-interfaces';
 import { gql } from '@apollo/client/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class KudosService {
-  kudos$: Observable<Kudos[]>;
-  constructor(private readonly apollo: Apollo) {
-    this.kudos$ = this.getKudos();
+  private readonly updateKudosSubject = new Subject<void>();
+
+  constructor(private readonly apollo: Apollo) {}
+
+  getKudos(username: string) {
+    return this.updateKudosSubject.asObservable().pipe(
+      startWith(''),
+      mapTo(true),
+      switchMap((refresh) => this.getKudosByUsername(username, refresh))
+    );
   }
 
-  private getKudos() {
+  giveKudos(input: GiveKudosInput) {
     return this.apollo
-      .query<{ me: { kudos: Kudos[] } }>({
+      .mutate({
+        mutation: gql`
+          mutation GiveKudos($userId: ID!, $content: JSON!) {
+            giveKudos(input: { userId: $userId, content: $content }) {
+              id
+            }
+          }
+        `,
+        variables: {
+          userId: input.userId,
+          content: input.content,
+        },
+      })
+      .pipe(tap(() => this.updateKudosSubject.next()));
+  }
+
+  private getKudosByUsername(username: string, refresh = false) {
+    return this.apollo
+      .query<{ userByUsername: { kudos: Kudos[] } }>({
         query: gql`
-          query GetMyKudos {
-            me {
+          query GetKudosByUsername($username: String!) {
+            userByUsername(username: $username) {
               kudos {
                 id
                 kudosBy {
@@ -34,7 +59,11 @@ export class KudosService {
             }
           }
         `,
+        variables: {
+          username,
+        },
+        fetchPolicy: refresh ? 'network-only' : 'cache-first',
       })
-      .pipe(map((result) => result.data.me.kudos));
+      .pipe(map((result) => result.data.userByUsername.kudos));
   }
 }
