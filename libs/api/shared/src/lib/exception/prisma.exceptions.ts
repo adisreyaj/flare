cf1a624ec52a408d956c354f369fe6ec19f641f1
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-02-24 17:58:20 +0530

    feat(api): add prisma exceptions filter

---
+++ libs/api/shared/src/lib/exception/prisma.exceptions.ts
@@ -0,0 +1,22 @@
+import { ArgumentsHost, Catch, HttpException, Logger } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { GqlExceptionFilter } from '@nestjs/graphql';
+
+const PRISMA_ERROR_CODE_TO_STATUS = {
+  P2025: {
+    code: 404,
+    message: 'Resource not found',
+  },
+};
+
+@Catch(PrismaClientKnownRequestError)
+export class PrismaExceptionFilter implements GqlExceptionFilter {
+  logger = new Logger(PrismaExceptionFilter.name);
+  catch(exception: PrismaClientKnownRequestError, host: ArgumentsHost) {
+    const { code, message } = PRISMA_ERROR_CODE_TO_STATUS[exception.code] ?? {
+      code: 500,
+      message: 'Internal server error',
+    };
+    return new HttpException(message, code);
+  }
+}
